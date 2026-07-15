8ef0f42023c275c1aebf923023fa548b82d5748b
Darshita Gondaliya <darshitagondaliya286@gmail.com>
2026-07-15 19:16:01 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,8 +23,8 @@ origins = [
     "http://localhost:5176",
     "http://localhost:5177",
     "http://localhost:3000",
-    "https://infinpost.onrender.com", 
     "https://infinpost-frontend1.onrender.com",  
+    "https://infinpost-frontend.onrender.com",  
 ]
 
 app.add_middleware(
